dd7880a28c37eb7ac8db40224b6fef2fa127cd80
EricBorges1 <ericborgeslg3@gmail.com>
2023-03-10 20:00:09 -0300

    Resolução do projeto nossoJogo 21

--- nossoJogo.js
+++ nossoJogo.js
@@ -10,3 +10,29 @@
  * 
  * 
  */
+function comecoDoJogo() {
+   if (confirm("Boas vindas ao jogo de BlackJack!\nQuer iniciar uma nova rodada?") == true) {
+      let cartasUsuario1 = comprarCarta()
+      let cartasUsuario2 = comprarCarta()
+      let cartasPc1 = comprarCarta()
+      let cartasPc2 = comprarCarta()
+      let calculoUsuario = cartasUsuario1.valor + cartasUsuario2.valor
+      let calculoPc = cartasPc1.valor + cartasPc2.valor
+
+      console.log(`Cartas do Usuário - ${cartasUsuario1.texto} e ${cartasUsuario2.texto} - ${calculoUsuario}`)
+      console.log(`Cartas do Computador - ${cartasPc1.texto} e ${cartasPc2.texto} - ${calculoPc}`)
+
+      if (calculoUsuario > calculoPc){
+         console.log('Usuário ganhou ! ')
+      }else if(calculoUsuario < calculoPc){
+         console.log('Computador ganhou ! ')
+      }else if(calculoUsuario === calculoPc){
+         console.log('EMPATE')
+      }
+   }
+   else {
+      console.log('O jogo acabou ! ')
+   }
+}
+comecoDoJogo()
+
