e41378062f6d2873cc06b987a0383f5576a8669b
gustaMACHO <henrique.gusta777@gmail.com>
2023-03-21 19:50:14 -0300

    projeto-nosso-jogo-finalizado

--- nossoJogo.js
+++ nossoJogo.js
@@ -10,3 +10,39 @@
  * 
  * 
  */
+   
+   
+    const blackjack = () => {
+      const boasVindas = 'Boas vindas ao jogo Black Jack!'
+      console.log(boasVindas)
+      
+      if(confirm('Quer iniciar uma nova rodada?')){
+         //variaveis que recebem as cartas
+         let carta1Usuario = comprarCarta()
+         let carta2Usuario = comprarCarta()
+         let carta1Computador = comprarCarta()
+         let carta2Computador = comprarCarta()
+         
+         //atribuir os valores as cartas
+         let pontuacaoUsuario = carta1Usuario.valor + carta2Usuario.valor
+         let pontuacaoComputador = carta1Computador.valor + carta2Computador.valor
+         
+         //console.log que exibe as pontuações
+         console.log(`usuário - ${carta1Usuario.texto} + ${carta2Usuario.texto} - ${pontuacaoUsuario}`)
+         console.log(`computador - ${carta1Computador.texto} + ${carta2Computador.texto} - ${pontuacaoComputador}`)        
+         
+         if(pontuacaoUsuario > pontuacaoComputador){
+            console.log('o usuário venceu!')
+         }else if(pontuacaoUsuario < pontuacaoComputador){
+            console.log('o computador venceu. Boa sorte na proxima tentativa!')
+         }else if(pontuacaoUsuario === pontuacaoComputador){
+            console.log('empate!')
+      }
+      
+      }else{
+         const cancelaJogo = 'O jogo acabou, esperamos vê-lo em breve!.'
+         console.log(cancelaJogo)
+   }
+   
+    }
+    blackjack()
