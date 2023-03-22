b39794371b8b2b1736e19bcb58eea83efe4efc23
Antonionunes03 <anunes17073@gmail.com>
2023-03-22 19:54:14 -0300

    Projeto-BlackJack-Finalizado

--- nossoJogo.js
+++ nossoJogo.js
@@ -1,12 +1,40 @@
-/**
- * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
- * 
- * 
-    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
-    
-    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
-    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
- * 
- * 
- * 
- */
+console.log('Bem-vindo(a) ao jogo de blackjack');
+
+const inicio = confirm('Quer iniciar uma nova rodada?');
+
+if (inicio) {
+    const primeiraCartaUser = comprarCarta();
+    const segundaCartaUser = comprarCarta();
+    const cartasUser = primeiraCartaUser.valor + segundaCartaUser.valor;
+
+    const primeiraCartaPC = comprarCarta();
+    const segundaCartaPC = comprarCarta();
+    const cartaPC = primeiraCartaPC.valor + segundaCartaPC.valor;
+
+    console.log(
+        'Cartas Do Usuário:',
+        primeiraCartaUser.texto,
+        segundaCartaUser.texto,
+        cartasUser
+    );
+    console.log(
+        'Cartas Do Computador',
+        primeiraCartaPC.texto,
+        segundaCartaPC.texto,
+        cartaPC
+    );
+
+    if (cartaPC > 21) {
+        console.log('O computador estourou! O usuario ganhou!');
+    } else if (cartasUser > 21) {
+        console.log('O Usuário estourou! O Computador Ganhou!');
+    } else if (cartaPC === cartasUser) {
+        console.log('Empatou!');
+    } else if (cartasUser > cartaPC) {
+        console.log('O Usuário ganhou!');
+    } else if (cartaPC > cartasUser) {
+        console.log('O Computador Ganhou!');
+    } else {
+        console.log('O Jogo Acabou!');
+    }
+}
