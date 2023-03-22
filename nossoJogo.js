d6c1e31ecead227a2fcd291fc8a50525c7aa119c
Cristian91Assuncao <cristian.assuncao@hotmail.com>
2023-03-22 19:08:30 -0300

    Nosso Jogo

--- nossoJogo.js
+++ nossoJogo.js
@@ -10,3 +10,40 @@
  * 
  * 
  */
+
+alert("Boas vindas ao jogo de BlackJack!")
+ if (confirm('Quer iniciar uma nova rodada?')) {
+   const primeiraCartaUsuario = comprarCarta()   
+   const segundaCartaUsuario = comprarCarta()
+   const somaDasCartasUsuario = primeiraCartaUsuario.valor + segundaCartaUsuario.valor
+   // primeiraCartaUsuario.texto imprime o texto da carta. Nesse caso: "K♦️" enquanto primeiraCartaUsuario.valor imprime o valor
+   
+   const resUsuario = `Usuário - cartas: ${primeiraCartaUsuario.texto} ${segundaCartaUsuario.texto} - Pontuação: ${somaDasCartasUsuario}`
+
+   console.log(resUsuario)
+
+
+   const primeiraCartaPc = comprarCarta()
+   const segundaCartaPc = comprarCarta()
+   const somaDasCartasPc = primeiraCartaPc.valor + segundaCartaPc.valor
+
+   const resPC = `Computador - cartas: ${primeiraCartaPc.texto} ${segundaCartaPc.texto} - Pontuação: ${somaDasCartasPc}`
+
+   console.log(resPC)
+
+
+   if (somaDasCartasUsuario > somaDasCartasPc) {
+      console.log('O usuário ganhou!')
+   } else if (somaDasCartasUsuario < somaDasCartasPc) {
+      console.log('O computador ganhou!')
+   } else {
+      console.log('Empate!')
+   }
+
+ } else {
+   alert('O jogo acabou!')
+ }
+
+
+ 
+// 4 - Abra um Pull Request e faça um commit com a frase “Nosso Jogo”.
