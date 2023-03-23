40912ac824afdcbb283c5fa32794437387e1a64a
jaozinmelo <joaopedrodemartins440@gmail.com>
2023-03-23 20:30:54 -0300

    Finalizado a versão do DESAFIO, estou muito contente, espero que gostem!

--- desafio.js
+++ desafio.js
@@ -11,3 +11,105 @@
  * 
  */
 
+
+
+function blackJackDesafio() {
+   //INICIANDO O JOGO
+   let confirmNovaRodada = confirm(`Bem-vinde ao jogo de Blackjack!\nQuer iniciar uma nova rodada?`)
+
+   if (confirmNovaRodada) {
+      //DISTRIBUINDO CARTAS
+      let jogadores = {
+         user: [comprarCarta(), comprarCarta()],
+         computador: [comprarCarta(), comprarCarta()]
+      }
+
+      // LÓGICA DO DUPLO ÁS (AVALIAR LÓGICA)
+      while (
+         (jogadores.user[0].valor === 11 && jogadores.user[1].valor === 11) || (jogadores.computador[0].valor === 11 && jogadores.computador[1].valor === 11)
+      ) {
+         jogadores = {
+            user: [],
+            computador: []
+         }
+      }
+      //ABREVIANDO O OBJETO      
+      let user = jogadores.user
+      let computador = jogadores.computador
+
+      //CRIANDO UM TEXTO PARA CONCATENAR COM OS ELEMENTOS EXTORNADOS PELO LAÇO(ESPERO QUE ENTENDA)
+      let cartaTextoUser = `Jogador: `
+      let cartaTextoComputador = `Computador: `
+
+      //DECLARANDO AS VARIÁVEIS DE PONTUAÇÃO
+      let pontosUser = 0
+      let pontosComputador = 0
+
+      //CRIANDO LAÇO PARA OBTER O VALOR DAS CARTAS
+      for (let i = 0; i < user.length; i++) {
+         pontosUser += user[i].valor
+      }
+      for (let i = 0; i < computador.length; i++) {
+         pontosComputador += computador[i].valor
+      }
+
+      //CRIANDO LAÇO PARA OBTER O "TEXTO" DAS CARTAS
+      for (let cartaTexto of jogadores.user) {
+         let nipe = cartaTexto.texto + " "
+         cartaTextoUser += nipe
+      }
+      for (let cartaTexto of jogadores.computador) {
+         let nipe = cartaTexto.texto + " "
+         cartaTextoComputador += nipe
+      }
+
+      //USUARIO COMPRANDO CARTAS
+      let comprando = true
+      while (comprando) {
+         if (pontosUser >= 21) {
+            break
+         }
+         let confirmCompra = confirm(`Suas cartas são ${cartaTextoUser}. \nA carta revelada do computador é ${jogadores.computador[0].texto}.\nDeseja comprar mais uma carta?`)
+
+         if (confirmCompra) {
+            let carta = comprarCarta()
+            jogadores.user.push(carta)
+            pontosUser += carta.valor
+            cartaTextoUser += `${carta.texto}`
+
+
+         } else {
+            comprando = false
+         }
+      }
+      //COMPUTADOR SÓ COMPRA CARTAS ENQUANTO AS CARTAS DO USUARIO FOREM < 21
+      while (pontosComputador <= pontosUser && pontosUser <= 21) {
+         let carta = comprarCarta()
+         jogadores.computador.push(carta)
+         pontosComputador += carta.valor
+         cartaTextoComputador += `${carta.texto}`
+      }
+
+      // LÓGICA DO RESULTADO
+      let resultado = ``
+      if (pontosUser > 21) {
+         resultado = `O computador ganhou!`
+      } else if (pontosComputador > 21) {
+         resultado = `O jogador ganhou!`
+      } else if (pontosUser < pontosComputador) {
+         resultado = `O computador ganhou`
+      } else if (pontosUser > pontosComputador) {
+         resultado = `O jogador ganhou!`
+      } else {
+         resultado = `Empate!`
+      }
+      //ALERT DO RESULTADO FINAL
+      alert(`${cartaTextoUser} -Pontuação: ${pontosUser}\n${cartaTextoComputador} -Pontuação: ${pontosComputador}\n${resultado}`)
+
+      //CASO O JOGADOR NÃO QUEIRA JOGAR
+   } else {
+      alert(`O jogo acabou.`)
+   }
+}
+
+blackJackDesafio()
--- nossoJogo.js
+++ nossoJogo.js
@@ -10,3 +10,45 @@
  * 
  * 
  */
+
+//boas vindas 
+console.log(`Boas vindas ao jogo de BlackJack!`)
+//CRIANDO A DISTRIBUIÇÃO DAS CARTAS
+const cartaUser1 = comprarCarta()
+const cartaUser2 = comprarCarta()
+const cartaPc1 = comprarCarta()
+const cartaPc2 = comprarCarta()
+//CRIANDO A VARIÁVEL COM O RESULTADO DA SOMA DAS 2 CARTAS
+const resultadoCartasUser = cartaUser1.valor + cartaUser2.valor 
+const resultadoCartasPc = cartaPc1.valor + cartaPc2.valor
+//LÓGICA
+function gameBlackJack (){
+//Criando frase
+const fraseNewGame = confirm(`Quer iniciar uma nova rodada?`)
+if (fraseNewGame) {
+   console.log(`Usuário - cartas: ${cartaUser1.texto} ${cartaUser2.texto} - ${resultadoCartasUser}`)
+   console.log(`Computador - cartas: ${cartaPc1.texto} ${cartaPc2.texto} - ${resultadoCartasPc}
+`)
+} else {
+   console.log(`O jogo acabou.`)
+}
+//LOGICA RESULTADO FINAL
+if (resultadoCartasUser > resultadoCartasPc) {
+   console.log(`O usuário ganhou!`)
+} else if (resultadoCartasUser === resultadoCartasPc) {
+   console.log(`Empate!`)
+} else {
+   console.log(`O computador ganhou!`)
+}
+//LOGICA DO ESTOURO
+if (resultadoCartasUser > 21){
+   console.log(`O usuario estourou, computador ganhou!`)
+} else if(resultadoCartasPc > 21){
+   console.log(`O computador estourou, usuário ganhou!`)
+} else if (resultadoCartasPc && resultadoCartasUser > 21){
+   console.log(`ESTOURO DUPLO!`)
+}
+}
+//
+gameBlackJack ()
+
