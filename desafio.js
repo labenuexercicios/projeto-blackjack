a234186976fab1c2c64c2ba3373f9cb689b6c269
ramonepico <ramonepico@gmail.com>
2023-03-24 20:02:40 -0300

    Projeto nosso desafio-Ramon

--- desafio.js
+++ desafio.js
@@ -9,47 +9,115 @@
  * 
  * 
  * 
- */ 
-/*function desafio21 (){
-let usuario = []
-let computador = []
+ */
 
-let iniciar = confirm(`Bem vindo(a) ao jogo de BlackJack!
-Quer iniciar uma nova rodada?`)
+//* As cartas dos jogadores (usuário e computador) podem ser armazenadas em array.
+function desafioBlackjack() {
+   let usuario = []
+   let computador = []
+   let inicio = confirm(`Boas vindas ao jogo de BlackJack! Quer iniciar uma nova jogada?`)
+   if(inicio === false){
+      return alert(`Você não iniciou o jogo`)
+   }
+   const cartaUsuario1 = usuario.push(comprarCarta())
+   const cartaUsuario2 = usuario.push(comprarCarta())
+   const cartaComputador1 = computador.push(comprarCarta())
+   const cartaComputador2 = computador.push(comprarCarta())
 
-   if(iniciar){
-      let cartasJogo = false
-      while(!iniciar){
-         usuario.push(comprarCarta())
-         usuario.push(comprarCarta())
-         computador.push(comprarCarta())
-         computador.push(comprarCarta())
-         if(usuario[0].valor === 11 && usuario[1].valor === 11 ||
-            computador[0].valor === 11 && computador[1].valor ){
+   const pontosDoUsuario = usuario
+   const pontosDoComputador = computador
+   
+   let result = confirm(`Suas cartas são ${usuario[cartaUsuario1 - 1].texto} + ${usuario[cartaUsuario2 - 1].texto}. A carta revelada do computador é ${computador[cartaComputador1 - 1].texto}. 
+      \n 
+      Deseja comprar mais uma carta?`)
+   let contador = 0
 
-            usuario = []
-            computador = []
-         }else{
-            cartasJogo = true
-         }
+   while (result) {
+      usuario.push(comprarCarta())
+
+      let contadorUsuario = 0
+      for (let carta of usuario) {
+         contadorUsuario = contadorUsuario + carta.valor
       }
-      computador[0].texto = `Carta do computador revelada`
-      computador[0].valor = 0
+      alert("Contador usuário: " + contadorUsuario)
+   
+      let contadorComputador = 0
+      for (let carta of computador) {
+         contadorComputador = contadorComputador + carta.valor
+      }
+      alert("Contador Computador: " + contadorComputador)
 
-      alert(`Usuário: ${usuario[0].texto} ${usuario[1].texto}
-      Computador: ${computador[0].texto} ${computador[1].texto}`)
-      
-
+      for(i = 0; i < usuario.length; i++){
+         contador = contador + usuario[i].valor
+      }
+      if(contador > 21){
+         result = false
+         return alert(`O computador ganhou!`)
+      }else {
+         result = confirm(`Deseja comprar mais uma carta?`)
+          alert(`Suas cartas são ${usuario[0].texto} ${usuario[1].texto}. Sua pontuação é ${pontosDoUsuario}  
+      "\n"  
+      As cartas do computador são ${computador[0].texto} ${computador[1].texto}.A pontuação do computador é ${pontosDoComputador}.` )
       }
    }
-desafio21 ()*/
-
-function desafio21 (){
-   confirm(`Bem vindo(a) ao jogo de BlackJack!
-Quer iniciar uma nova rodada?`)
-   const carta = comprarCarta() 
    
+   let contadorUsuario = 0
+   for (let carta of usuario) {
+      contadorUsuario = contadorUsuario + carta.valor
+   }
+   alert("Contador usuário: " + contadorUsuario)
+
+   let contadorComputador = 0
+   for (let carta of computador) {
+      contadorComputador = contadorComputador + carta.valor
+   }
+   alert("Contador Computador: " + contadorComputador)
+
+   if (contadorUsuario > contadorComputador) {
+      return alert(`Você ganhou!`)
+   } else if (contadorUsuario === contadorComputador){
+      return alert(`Você empatou com o computador!`)
+   } else {
+      return alert(`O computador ganhou!`)
+   }
+
+   
+
+   
+
+
+   //Se as duas cartas iniciais do usuário ou do computador forem dois ases (A), as cartas devem ser sorteadas novamente.
+   /*if (comprarCarta === 2 * "A") {
+      return `Fim do Jogo`
+   }*/
+
+   //Após o sorteio das 2 cartas para cada jogador, as duas primeiras cartas do usuário continuam 
+   //sendo reveladas. A primeira carta do computador é revelada, a segunda é oculta por enquanto. 
+   //Você deve perguntar ao usuário se ele deseja comprar mais uma carta. Veja abaixo:
+
+
+   /*if(ok){
+       return usuario = usuario + comprarCarta()
+
+    } else {
+       return `Fim de Jogo`
+    }*/
+
+
+
+
+  
+
 }
+desafioBlackjack()
+
+
+
+
+
+
+
+
 
 
 
