98fc28a5e19cb52283be3a64832322e1dfdb3e77
João Neiva <jg.neiva@gmail.com>
2023-03-29 20:34:29 -0300

    Nosso Jogo

--- nossoJogo.js
+++ nossoJogo.js
@@ -10,3 +10,32 @@
  * 
  * 
  */
+
+console.log("Boas vindas ao jogo de BlackJack!")
+
+if (confirm("Quer iniciar uma nova rodada?") === true){
+   const cartaUsuario1 = comprarCarta()
+   const cartaUsuario2 = comprarCarta()
+   const somaUsuario = cartaUsuario1.valor + cartaUsuario2.valor
+
+   const cartaComputador1 = comprarCarta()
+   const cartaComputador2 = comprarCarta()
+   const somaComputador = cartaComputador1.valor + cartaComputador2.valor
+
+   console.log(`Usuário - cartas: ${cartaUsuario1.texto} ${cartaUsuario2.texto} - pontuação ${somaUsuario}`)
+   console.log(`Computador - cartas: ${cartaComputador1.texto} ${cartaComputador2.texto} - pontuação ${somaComputador}`)
+
+   if ((somaComputador === somaUsuario) || (somaComputador>21 && somaUsuario>21)){
+      console.log("Empate!")
+   }
+   else if ((somaUsuario > somaComputador && somaUsuario<=21) || somaComputador>21){
+      console.log("O usuário ganhou!")
+   }
+   else {
+      console.log("O computador ganhou!")
+   }
+
+}
+else {
+   console.log("O jogo acabou")
+}
