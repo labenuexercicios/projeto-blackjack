088087eeddab98549d2817dfce5a41ca85295fa2
carolkapelinski <carolkapelinkski@gmail.com>
2023-03-30 18:59:10 -0300

    Nosso Jogo

--- nossoJogo.js
+++ nossoJogo.js
@@ -10,3 +10,29 @@
  * 
  * 
  */
+    console.log("Bem-vinde ao jogo de blackjack!")
+
+    let jogo = confirm("Quer iniciar uma nova rodada?")
+    if (jogo) {
+        let carta1Usuario = comprarCarta()
+        let carta2Usuario = comprarCarta()
+        let carta1Computador = comprarCarta()
+        let carta2Computador = comprarCarta()
+    
+        let pontuacaoUsuario = carta1Usuario.valor + carta2Usuario.valor
+        let pontuacaoComputador = carta1Computador.valor + carta2Computador.valor
+    
+        console.log(`Usuário - cartas: ${carta1Usuario.texto} ${carta2Usuario.texto} - ${pontuacaoUsuario}`)
+        console.log(`Computador - cartas: ${carta1Computador.texto} ${carta2Computador.texto} - ${pontuacaoComputador}`)
+    
+        if (pontuacaoUsuario > pontuacaoComputador) {
+            console.log("O usuário ganhou!")
+        } else if (pontuacaoComputador > pontuacaoUsuario) {
+            console.log("O computador ganhou!")
+        } else if (pontuacaoUsuario === pontuacaoComputador) {
+            console.log("Empate!")
+        }
+    }
+    else {
+        console.log("O jogo acabou!")
+    }
