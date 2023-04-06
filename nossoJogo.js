dd28e984d3a42535a7e7b420913136dc982ce21c
joaosantosvictor <joaovictormedia@gmail.com>
2023-04-06 16:03:30 -0300

    primeiro-commit-blackback

--- nossoJogo.js
+++ nossoJogo.js
@@ -10,3 +10,66 @@
  * 
  * 
  */
+
+console.log("Boas vindas ao jogo BlackJack!")
+const inicio = confirm("Quer iniciar uma nova rodada?")
+
+const jogadorCarta1 = comprarCarta()
+const jogadorCarta2 = comprarCarta()
+const jogadorMaisUma = comprarCarta()
+const jogadorTotal = jogadorCarta1.valor + jogadorCarta2.valor
+const jogadorTotalMaisUminha = jogadorCarta1.valor + jogadorCarta2.valor + jogadorMaisUma.valor
+
+
+const pcCarta1 = comprarCarta()
+const pcCarta2 = comprarCarta()
+const pcMaisUma = comprarCarta()
+const pcTotal = pcCarta1.valor + pcCarta2.valor
+const pcTotalMaisUminha = pcCarta1.valor + pcCarta2.valor + pcMaisUma.valor
+
+
+
+if (inicio === true) {
+   console.log("Usuario - Cartas: ", jogadorCarta1.texto, "-Pontuação:", jogadorCarta1.valor)
+   console.log("Computador - Cartas: ", pcCarta1.texto, "-Pontuação: ", pcCarta1.valor)
+
+   if (confirm("Quer mais uma carta?")) {
+
+
+      console.log("Usuario - Cartas: ", jogadorCarta1.texto, jogadorCarta2.texto, "-Pontuação:", jogadorCarta1.valor + jogadorCarta2.valor)
+      console.log("Computador - Cartas: ", pcCarta1.texto, pcCarta2.texto, "-Pontuação: ", pcCarta1.valor + pcCarta2.valor)
+
+      if (confirm("Mais uminha?")) {
+
+         console.log("Usuario - Cartas: ", jogadorCarta1.texto, jogadorCarta2.texto, jogadorMaisUma.texto, "-Pontuação:", jogadorCarta1.valor + jogadorCarta2.valor + jogadorMaisUma.valor)
+         console.log("Computador - Cartas: ", pcCarta1.texto, pcCarta2.texto, pcMaisUma.texto, "-Pontuação: ", pcCarta1.valor + pcCarta2.valor + pcMaisUma.valor)
+         
+         if (jogadorTotalMaisUminha > pcTotalMaisUminha && jogadorTotalMaisUminha <= 21) {
+            console.log('Jogador ganhou.')
+         } else if (jogadorTotalMaisUminha === pcTotalMaisUminha) {
+            console.log('Jogo empatado.')
+         } else if (pcTotalMaisUminha > jogadorTotalMaisUminha && pcTotalMaisUminha <= 21) {
+            console.log("O computador ganhou.")
+         }
+
+      } else {
+         if (jogadorTotal > pcTotal && jogadorTotal <= 21) {
+            console.log('Jogador ganhou.')
+         } else if (jogadorTotal === pcTotal) {
+            console.log('Jogo empatado.')
+         } else if (pcTotal > jogadorTotal && pcTotal <= 21) {
+            console.log("O computador ganhou.")
+         }
+      }
+   } else {
+      if(jogadorCarta1.valor > pcCarta1.valor && jogadorCarta1.valor <= 21) {
+         console.log("Jogador ganhou.")
+      }else if (jogadorCarta1.valor === pcCarta1.valor) {
+         console.log("Jogo empatado.")
+      } else if (pcCarta1.valor > jogadorCarta1.valor && pcCarta1.valor <= 21) {
+         console.log("O computador ganhou.")
+      }
+   }
+} else {
+   console.log("O jogo acabou.")
+} 
