005b9755e3b0cf00ee4204e09f6c088d6ba8c71b
leosilvapb <leo-leonardo-silva@hotmail.com>
2023-04-06 23:33:22 -0300

    Projeto finalizado

--- desafio.js
+++ desafio.js
@@ -1,13 +1,104 @@
-/**
- * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
- * 
- * 
-    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
-    
-    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
-    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
- * 
- * 
- * 
- */
 
+let check = true
+
+let userCartas = []
+let pcCartas = []
+
+let pontosUser = 0
+let pontosPc = 0
+
+let userMao
+let pcMao
+
+
+const sorteioCartas = () => {
+   for (let i = 0; i < 2; i++) {
+      userCartas.push(comprarCarta())
+   }
+
+   for (let i = 0; i < 2; i++) {
+      pcCartas.push(comprarCarta())
+   }
+}
+
+const verificaCarta = () => {
+   if (userCartas[0].texto === "A" && userCartas[1].texto === "A") {
+      userCartas = []
+      pcCartas = []
+      sorteioCartas()
+   } else if (pcCartas[0].texto === "A" && pcCartas[1].texto === "A") {
+      userCartas = []
+      pcCartas = []
+      sorteioCartas()
+   }
+}
+
+const somaPontos = () => {
+   pontosUser = 0
+   pontosPc = 0
+   for (let i = 0; i < userCartas.length; i++) {
+      pontosUser += userCartas[i].valor
+   }
+   for (let i = 0; i < pcCartas.length; i++) {
+      pontosPc += pcCartas[i].valor
+   }
+}
+
+const lerUserMao = () => {
+   userMao = ""
+   for (const i of userCartas) {
+      userMao += i.texto + ""
+   }
+}
+
+const lerPcMao = () => {
+   pcMao = ""
+   for (const i of pcCartas) {
+      pcMao += i.texto + ""
+   }
+}
+
+const novaRodada = () => {
+   if (confirm(`Suas cartas são ${userMao}. \nA carta revelada do computador é ${pcCartas[0].texto}.\nDeseja comprar mais uma carta?.`)) {
+      userCartas.push(comprarCarta())
+      lerUserMao()
+      somaPontos()
+   } else {
+      while (pontosPc < pontosUser) {
+         pcCartas.push(comprarCarta())
+         lerPcMao()
+         somaPontos()
+      }
+      check = false
+   }
+}
+
+const resultado = () => {
+   let mensagem = ""
+   if (pontosUser === pontosPc) {
+      mensagem = "Empate"
+   } else if (pontosPc > 21 || (pontosUser > pontosPc && pontosUser <= 21)) {
+      mensagem = "Você venceu!"
+   } else if (pontosUser > 21 || (pontosPc > pontosUser && pontosPc <= 21)) {
+      mensagem = "O computador Venceu!"
+   }
+   alert(`Suas cartas são ${userMao}. Sua pontuação é ${pontosUser}.\nAs cartas do computador são ${pcMao}. A pontuação do computador é ${pontosPc}.\n${mensagem}`)
+}
+
+const startGame = () => {
+   if (confirm("Boas vindas ao jogo de BlackJack!" + "\n\nDeseja iniciar um novo jogo?")) {
+      sorteioCartas()
+      verificaCarta()
+      somaPontos()
+      lerUserMao()
+      lerPcMao()
+
+      while (pontosUser < 21 && check === true) {
+         novaRodada()
+      }
+      resultado()
+   } else {
+      alert("O jogo acabou!")
+   }
+}
+startGame()
--- nossoJogo.js
+++ nossoJogo.js
@@ -1,12 +1,59 @@
-/**
- * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
- * 
- * 
-    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
-    
-    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
-    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
- * 
- * 
- * 
- */
+// /**
+//  * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+//  * 
+//  * 
+//     let carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+
+//     console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+//     console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+//  * 
+//  * 
+//  * 
+//  */
+
+// let carta = comprarCarta()
+
+console.log("Boas vindas ao jogo de BlackJack!")
+
+
+if (confirm("Quer iniciar uma nova rodada?")) {
+
+
+   let userCarta1 = comprarCarta()
+   let userCarta2 = comprarCarta()
+
+   let pcCarta1 = comprarCarta()
+   let pcCarta2 = comprarCarta()
+
+   let somaUser = userCarta1.valor + userCarta2.valor
+   let somaPc = pcCarta1.valor + pcCarta2.valor
+
+   if (somaUser === 22 || somaPc === 22) {
+
+      userCarta1 = comprarCarta()
+      userCarta2 = comprarCarta()
+
+      pcCarta1 = comprarCarta()
+      pcCarta2 = comprarCarta()
+
+      somaUser = userCarta1.valor + userCarta2.valor
+      somaPc = pcCarta1.valor + pcCarta2.valor
+   }
+
+   console.log(`Usuário - Cartas ${userCarta1.texto} ${userCarta2.texto} - Pontuação ${somaUser}`)
+   console.log(`computador - Cartas ${pcCarta1.texto} ${pcCarta2.texto} - Pontuação ${somaPc}`)
+
+   if (somaUser > somaPc) {
+      console.log("O usuário venceu!")
+
+   } else if (somaUser < somaPc) {
+      console.log("O computador venceu!")
+
+   } else {
+      console.log("Empate")
+   }
+
+
+} else {
+   console.log("O jogo acabou")
+}
