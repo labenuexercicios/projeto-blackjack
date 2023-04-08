122e3650255a6c8e6b62ecc16953931331b4a6b6
rafaelmelodruzian <rmdconsultor@gmail.com>
2023-04-07 21:47:53 -0300

    Nosso jogo

--- nossoJogo.js
+++ nossoJogo.js
@@ -1,12 +1,37 @@
-/**
- * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
- * 
- * 
-    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
-    
-    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
-    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
- * 
- * 
- * 
- */
+console.log("Boas vindas ao jogo de BlackJack!")
+const querJogar = confirm("Quer iniciar uma nova rodada?")
+let cartasUsu = []
+let cartasComp = []
+let valorComp = 0
+let valorUsu = 0
+let textoUsu = ""
+let textoComp = ""
+
+if (querJogar !== false) {
+    for (i = 0; i <= 1; i++) {
+        cartasComp.push(comprarCarta())
+        valorComp += cartasComp[i].valor
+        textoComp += " "+cartasComp[i].texto
+    }
+    for (j = 0; j <= 1; j++) {
+        cartasUsu.push(comprarCarta());
+        valorUsu += cartasUsu[j].valor
+        textoUsu += " "+cartasUsu[j].texto
+    }
+
+    if (valorUsu > valorComp && valorUsu <= 21) {
+        vencedor = "Você ganhou! Parabéns!"
+    } else if (valorUsu < valorComp && valorComp <= 21) {
+        vencedor = "O computador ganhou!"
+    } else if (valorComp > 21 && valorUsu <= 21) {
+        vencedor = "Você ganhou! Parabéns!"
+    } else if (valorUsu > 21 && valorComp <= 21) {
+        vencedor = "O computador ganhou!"
+    } else {
+        vencedor = "Usuario e computador empataram!"
+    }
+}
+console.log(`Suas cartas são: ${textoUsu}. Pontuação: ${valorUsu} pontos. 
+Cartas do computador: ${textoComp}. Pontuação: ${valorComp} pontos.
+${vencedor}`)
+console.log(`Fim do jogo! Obrigado e volte sempre!`)
