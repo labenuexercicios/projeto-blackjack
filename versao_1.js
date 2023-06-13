37fd7bdb82cca648647031f385b8e19a78698ee3
Wellison <wellison.borges.97@gmail.com>
2023-06-13 11:14:25 -0300

    Blackjack versão 1.0

--- versao_1.js
+++ versao_1.js
@@ -10,3 +10,31 @@
  * 
  * 
  */
+
+    console.log("Bem-vindo(a) ao jogo de blackjack")
+
+    let jogo = confirm("Quer iniciar uma nova rodada?")
+    
+    if (jogo) {
+        let carta1Usuario = comprarCarta()
+        let carta2Usuario = comprarCarta()
+        let carta1Pc = comprarCarta()
+        let carta2Pc = comprarCarta()
+    
+        let pontuacaoUsuario = carta1Usuario.valor + carta2Usuario.valor
+        let pontuacaoPc = carta1Pc.valor + carta2Pc.valor
+    
+        console.log(`Usuário - cartas: ${carta1Usuario.texto} ${carta2Usuario.texto} - ${pontuacaoUsuario}`)
+        console.log(`Computador - cartas: ${carta1Pc.texto} ${carta2Pc.texto} - ${pontuacaoPc}`)
+    
+        if (pontuacaoUsuario > pontuacaoPc) {
+            console.log("O usuário ganhou!")
+        } else if (pontuacaoPc > pontuacaoUsuario) {
+            console.log("O computador ganhou!")
+        } else if (pontuacaoUsuario === pontuacaoPc) {
+            console.log("Empate!")
+        }
+    
+    } else {
+        console.log("O jogo acabou")
+    }
