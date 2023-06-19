f99fa7aa53fd6336d4b01995b43abf0bfd69d371
Giovanni Souza <GSMartins999>
2023-06-19 15:30:47 -0300

    parte1blackjack

--- versao_1.js
+++ versao_1.js
@@ -1,12 +1,28 @@
-/**
- * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
- * 
- * 
-    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
-    
-    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
-    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
- * 
- * 
- * 
- */
+if(ganhador){
+// Usuário comprou: 
+        let usuarioCarta1 = comprarCarta()
+        let usuarioCarta2 = comprarCarta()
+
+        // PC comprou:
+        let computadorCarta01 = comprarCarta()
+        let computadorCarta02 = comprarCarta()
+
+
+        //Soma das cartas: 
+        let somaUsuario = usuarioCarta1.valor + usuarioCarta2.valor
+        let somaComputador = computadorCarta01.valor + computadorCarta02.valor
+
+        console.log(`Usuário - cartas: ${usuarioCarta1.texto} ${usuarioCarta2.texto} - ${somaUsuario}`)
+        console.log(`Computador - cartas: ${computadorCarta01.texto} ${computadorCarta02.texto} - ${somaComputador}`)
+
+
+        if(somaUsuario > somaComputador){
+        console.log("Usuario ganhou")
+        } else if(somaUsuario === somaComputador){
+            console.log("Empate.")
+        }else if(somaComputador > somaUsuario){
+            console.log("Computador ganhou")
+        }
+    }else {
+        console.log("O jogo Acabou!")
+}   
