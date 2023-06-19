d00a311a08bf7bdfd99f49df5d60c4a05d15e3cc
Douglasluis508 <douglasluis508@gmail.com>
2023-06-19 17:08:45 -0300

    blackjack versão1

--- versao_1.js
+++ versao_1.js
@@ -10,3 +10,32 @@
  * 
  * 
  */
+
+    
+    console.log("Boas vindas ao jogo de Blackjack!")
+
+    let iniciarRodada = confirm("Quer iniciar uma nova rodada?")
+    
+    if (iniciarRodada) {
+       let usuario = comprarCarta().valor + comprarCarta().valor
+       let computador = comprarCarta().valor + comprarCarta().valor
+    
+       console.log(`Usuário - Cartas: ${comprarCarta().texto}, ${comprarCarta().texto} - pontuação: ${usuario}`)
+       console.log(`Computador - Cartas: ${comprarCarta().texto}, ${comprarCarta().texto} - pontuação: ${computador}`)
+    
+       if (usuario === computador) {
+          console.log("Empate.")
+       } else if (usuario > computador && usuario <= 21) {
+          console.log("O usuário ganhou.")
+       } else if (computador > usuario && computador <= 21) {
+          console.log("O computador ganhou.")
+       } else if (usuario > 21 && computador <= 21) {
+          console.log("O computador ganhou.")
+       } else if (computador > 21 && usuario <= 21) {
+          console.log("O usuário ganhou.")   
+       }
+    } else {
+       console.log("O jogo acabou.")
+    }
+    
+    
