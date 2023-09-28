3b1610e92443e2d3d9f4beed88500eda3b2563c5
raissanfg <99144281+raissanfg@users.noreply.github.com>
2023-09-28 08:19:03 -0500

    “Implementações-do-projeto”

--- versao_1.js
+++ versao_1.js
@@ -10,3 +10,32 @@
  * 
  * 
  */
+
+
+console.log("Bem-vindo(a) ao jogo de blackjack");
+
+const querJogar = confirm("Deseja iniciar uma nova rodada?")
+
+const usuario1 = comprarCarta();
+const usuario2 = comprarCarta();
+const computador1 = comprarCarta();
+const computador2 = comprarCarta();
+
+const somaUsuario = usuario1.valor + usuario1.valor
+const somaComputador = computador1.valor + computador2.valor
+
+if (querJogar) {
+   console.log(`Usuário - cartas:  ${usuario1.texto} ${usuario1.texto}  -  Pontuaçäo ${somaUsuario}`)
+   console.log(`Computador - cartas:  ${computador1.texto} ${computador2.texto} - Pontuaçäo ${somaComputador}`)
+
+   if (somaUsuario > somaComputador) {
+      console.log(" O usuário ganhou!")
+   } else if (somaComputador > somaUsuario) {
+      console.log("O computador ganhou!")
+   } else if (somaUsuario === somaComputador) {
+      console.log("Empate!")
+   } else {
+      console.log("O jogo acabou")
+   }
+
+}
