a2d04210addac9d7c4a0fc26d09b07bbc3a5a273
Eduardo Biskup <eduardobiskup@gmail.com>
2023-12-03 13:27:26 -0300

    projeto-BlackJack-versão2.0

--- versao_1.js
+++ versao_1.js
@@ -1,12 +1,31 @@
-/**
- * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
- * 
- * 
-    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+
+console.log('Bem-vindo ao blackjack! Boa sorte no jogo!')
+
+let jogo = confirm('Vamos jogar uma nova rodada?')
+
+if (jogo) {
+    let carta1Usuario = comprarCarta()
+    let carta2Usuario = comprarCarta()
+    let carta1Adversario = comprarCarta()
+    let carta2Adversario = comprarCarta()
+
+    let pontuacaoUsuario = carta1Usuario.valor + carta2Usuario.valor
+    let pontuacaoAdversario = carta1Adversario.valor + carta2Adversario.valor
+
+    console.log(`Usuário: cartas: ${carta1Usuario.texto} ${carta2Usuario.texto} = ${pontuacaoUsuario}`)
+    console.log(`Adversário: cartas: ${carta1Adversario.texto} ${carta2Adversario.texto} = ${pontuacaoAdversario}`)
+
+    if (pontuacaoUsuario > pontuacaoAdversario) {
+        console.log('O usuário ganhou!')
     
-    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
-    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
- * 
- * 
- * 
- */
+    } else if (pontuacaoAdversario > pontuacaoUsuario) {
+        console.log('O computador ganhou!')
+    
+    } else if (pontuacaoUsuario === pontuacaoAdversario) {
+        console.log('Empate!')
+    }
+
+} else {
+    console.log("O jogo acabou")
+}
+
