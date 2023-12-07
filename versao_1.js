f847b2660b1f73082bca65dea38c45febbf9c998
LuizFernandoForte <lfforte@gmail.com>
2023-12-07 10:26:21 -0300

    Blackjack versão 1.0

--- versao_1.js
+++ versao_1.js
@@ -10,3 +10,30 @@
  * 
  * 
  */
+console.log(`Boas vindas ao jogo de BlackJack!`);
+
+if (confirm('Quer iniciar uma nova rodada?')) {
+   // cartas Usuário
+   const cartaUsuario1 = comprarCarta();
+   const cartaUsuario2 = comprarCarta();
+   let valorTotalUsuario = cartaUsuario1.valor + cartaUsuario2.valor;
+   console.log(`Usuário - Cartas: ${cartaUsuario1.texto} - ${cartaUsuario2.texto} - pontuação ${valorTotalUsuario}`);
+
+   // cartas Computador
+   const cartaMaq1 = comprarCarta();
+   const cartaMaq2 = comprarCarta();
+   let valorTotalMaq = cartaMaq1.valor + cartaMaq2.valor;
+   console.log(`Computador - Cartas: ${cartaMaq1.texto} - ${cartaMaq2.texto} - pontuação ${valorTotalMaq}`);
+
+   if (valorTotalUsuario > valorTotalMaq) {
+      console.log('O usuário ganhou!');
+   } else if (valorTotalUsuario === valorTotalMaq) {
+      console.log('Empate!');
+   } else {
+      console.log('O computador ganhou!');
+   }
+
+
+} else {
+   console.log(`O jogo acabou!`);
+};
